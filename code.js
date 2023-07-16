6399c31802561bd0c73478757d9a373e3f4bdd1e
Kunal Kumar <Kunal.lohano@gmail.com>
2023-07-17 00:16:01 +0500

    Add option for chossing the number of blocks

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
 const content = document.createElement('div');
 content.classList.add('window');
 const container = document.querySelector('.container');
-
-for(i=0; i<16; i++){
-    for(j=0; j<16; j++){
-        const content = document.createElement('div');
-        content.classList.add('window');
-        container.appendChild(content);
+makeGrid(16,16);
+function makeGrid(i,j){
+    for(i=0; i<16; i++){
+        for(j=0; j<16; j++){
+            const content = document.createElement('div');
+            content.classList.add('window');
+            container.appendChild(content);
+        }
     }
 }
 
@@ -24,6 +26,39 @@ block.forEach(key => key.addEventListener('mouseover', function(e){
     e.target.style.background=`rgb(${color[0]}, ${color[1]}, ${color[2]})`;
 }));
 
-// block.addEventListener('mouseover',function (e){
-//     e.target.style.background='blue';
-// });
+const submit = document.querySelector('.submit');
+
+submit.addEventListener('click',()=>{
+    const userInput = document.querySelector(".input").value;
+    // console.log(+userInput);
+    // console.log(typeof(userInput));
+    input = parseInt(userInput);
+    // console.log(typeof(numb));
+    size = (800/userInput)-2;
+    const block = document.getElementsByClassName('window');
+    //block.remove();
+
+    while (block.length > 0) {
+        block[0].parentNode.removeChild(block[0]);
+    }
+
+    printGrid(input,input,size);
+    const blocks = document.querySelectorAll('.window');
+
+blocks.forEach(key => key.addEventListener('mouseover', function(e){
+    color = getColor();
+    e.target.style.background=`rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+}));
+});
+
+function printGrid(a,b,size){
+    for(i=0; i<a; i++){
+        for(j=0; j<b; j++){
+            const content = document.createElement('div');
+            content.classList.add('window');
+            content.style.width='${size}px';
+            content.style.height = '${size}px';
+            container.appendChild(content);
+        }
+    }
+}
